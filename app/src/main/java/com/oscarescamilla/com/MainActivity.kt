083600e27640c0ecf3401553ef842fc9d608116e
unknown <escamillaluquenoo@gmail.com>
2020-09-16 20:23:08 -0500

    config navigation components finished

--- app/src/main/java/com/oscarescamilla/com/MainActivity.kt
+++ app/src/main/java/com/oscarescamilla/com/MainActivity.kt
@@ -2,10 +2,27 @@ package com.oscarescamilla.com
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var  navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.nav_host_fragment) // hace referencia al fragment root
+        NavigationUI.setupActionBarWithNavController(this, navController) // habilitamos el boton de regreso indicando de que fragment depende
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        // sobre escribimos con nuestro navController
+        return navController.navigateUp()
+    }
+
+
 }
--- app/src/main/java/com/oscarescamilla/com/ui/MainFragment.kt
+++ app/src/main/java/com/oscarescamilla/com/ui/MainFragment.kt
@@ -1,17 +1,19 @@
 package com.oscarescamilla.com.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.oscarescamilla.com.R
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
@@ -20,6 +22,15 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_detalle.setOnClickListener {
+            findNavController().navigate(R.id.drinkDetailFragment)
+        }
     }
 
 
