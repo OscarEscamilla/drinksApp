c520fb05f36a36c50c1e748783d7e63d198ec8ac
OscarEscamilla <escamillaluquenoo@gmail.com>
2022-06-09 14:11:28 -0500

    update in app funcionando con prueba interna

--- app/src/main/java/com/drinks/com/MainActivity.kt
+++ app/src/main/java/com/drinks/com/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.drinks.com
 
+import android.app.Activity
 import android.content.Intent
 import android.content.IntentSender
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +16,7 @@ import com.google.android.play.core.appupdate.AppUpdateInfo
 import com.google.android.play.core.appupdate.AppUpdateManager
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
 import com.google.android.play.core.appupdate.testing.FakeAppUpdateManager
+import com.google.android.play.core.install.model.ActivityResult
 import com.google.android.play.core.install.model.AppUpdateType
 import com.google.android.play.core.install.model.UpdateAvailability
 import com.google.android.play.core.tasks.Task
@@ -23,12 +25,9 @@ import java.lang.Exception
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
-
-
-    private val APP_UPDATE_TYPE_SUPPORTED = AppUpdateType.IMMEDIATE
-
-    private  val REQUEST_UPDATE = 100
-
+    private lateinit var appUpdateManager: AppUpdateManager
+    val REQUEST_CODE = 100
+    val TAG = "IAU"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,49 +51,84 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun checkForUpdates() {
+        appUpdateManager = AppUpdateManagerFactory.create(baseContext)
 
-        val REQUEST_CODE = 100
-        val TAG = "IAU"
-        val appUpdateManager = AppUpdateManagerFactory.create(baseContext)
+        appUpdateManager.appUpdateInfo
+            .addOnSuccessListener { appUpdateInfo ->
+                Log.e(TAG, appUpdateInfo.updateAvailability().toString())
 
-        // Returns an intent object that you use to check for an update.
-        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
-
-        // Checks that the platform will allow the specified type of update.
-        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
-            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
-                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
-            ) {
-                toast("Update Disponible")
-                try {
+                if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                    && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
+                ) {
+                    toast("Update Disponible")
                     appUpdateManager.startUpdateFlowForResult(
                         appUpdateInfo,
                         AppUpdateType.IMMEDIATE,
                         this,
-                        REQUEST_CODE)
-                }catch (e: IntentSender.SendIntentException){
-                    toast(e.message.toString())
-                    Log.e(TAG, e.message.toString())
+                        REQUEST_CODE
+                    )
+                } else {
+                    toast("Update NO Disponible")
                 }
-            }else{
-                toast("Update NO Disponible")
+            }.addOnFailureListener {
+                toast(it.message.toString())
+                Log.e(TAG, it.message.toString())
             }
-        }
-
-        appUpdateInfoTask.addOnFailureListener {
-            toast(it.message.toString())
-            Log.e(TAG, it.message.toString())
-        }
 
     }
 
-
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        // super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE) {
+            when (resultCode) {
+                Activity.RESULT_OK -> {
+                    Log.d(TAG, "Result Ok")
+                    //  handle user's approval }
+                }
+                Activity.RESULT_CANCELED -> {
+                    run {
+                        //if you want to request the update again just call checkUpdate()
+                    }
+                    Log.d(TAG, "Result Cancelled")
+                    //  handle user's rejection  }
+                }
+                ActivityResult.RESULT_IN_APP_UPDATE_FAILED -> {
+                    //if you want to request the update again just call checkUpdate()
+                    Log.d(TAG, "Update Failure")
+                    //  handle update failure
+                }
+            }
+        }
+    }
 
 
     // rewrite back button
     override fun onSupportNavigateUp(): Boolean {
         // sobre escribimos con nuestro navController
         return navController.navigateUp()
+    }
+
+    // Checks that the update is not stalled during 'onResume()'.
+    // However, you should execute this check at all entry points into the app.
+    override fun onResume() {
+        super.onResume()
+
+        appUpdateManager
+            .appUpdateInfo
+            .addOnSuccessListener { appUpdateInfo ->
+
+                if (appUpdateInfo.updateAvailability()
+                    == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS
+                ) {
+                    // If an in-app update is already running, resume the update.
+                    appUpdateManager.startUpdateFlowForResult(
+                        appUpdateInfo,
+                        AppUpdateType.IMMEDIATE,
+                        this,
+                        REQUEST_CODE
+                    )
+                }
+            }
     }
 
 
